46564ea1f81579a3a2604f95a548a46e9e889a76
mmgumus <mahmut_muhammet@hotmail.com>
2023-12-29 12:44:00 +0300

    update

--- CheckParallels.js
+++ CheckParallels.js
@@ -1,47 +1,48 @@
-// Aşağıda istenilen bilgilere göre yazılımsal olarak bir çözüm çıkartmanız beklenmektedir? Teknoloji olarak kıstas yoktur, istenilen platformda yapılabilir
-// Doğrusal denklem sistemimizde noktalarımız var;
-// a) (1;2) , (3;7) , (0;6) , (1;1) , (-5,2), (1,5) , (3,-5)........
-// Bu kümede kaç tane doğru y eksenine paraleldir, kaç tane doğru x eksenine paraleldir? ( Aynı noktalardan geçen doğrular tek olarak değerlendirilecek )
-
-// Verilen n sayısı kadar x ve y koordinatlarına sahip rastgele nokta oluşturuyorum.
-function generateRandomPoints(numberOfPoints) {
-   let points = [];
-   for (let i = 0; i < numberOfPoints; i++) {
-      let x = Math.floor(Math.random() * 10);
-      let y = Math.floor(Math.random() * 10);
-      points.push({ x, y });
-   }
-   return points;
-}
-
-function checkLineParallels(points) {
-   let parallelToXCount = 0;
-   let parallelToYCount = 0;
+function findParallelLines(points) {
+   let parallelCountX = 0;
+   let parallelCountY = 0;
    let seenLines = new Set();
 
-   for (let i = 0; i < points.length - 1; i++) {
-      let lineKeyX = `x:${points[i].x}-${points[i + 1].x}`;
-      let lineKeyY = `y:${points[i].y}-${points[i + 1].y}`;
+   for (let i = 0; i < points.length; i++) {
+      for (let j = i + 1; j < points.length; j++) {
+         let lineKeyY = `x:${points[i].x}-${points[j].x}`;
+         let lineKeyX = `y:${points[i].y}-${points[j].y}`;
 
-      if (points[i].y === points[i + 1].y && !seenLines.has(lineKeyY)) {
-         parallelToXCount++;
-         seenLines.add(lineKeyY);
-      }
-      if (points[i].x === points[i + 1].x && !seenLines.has(lineKeyX)) {
-         parallelToYCount++;
-         seenLines.add(lineKeyX);
+         if (points[i].y === points[j].y && !seenLines.has(lineKeyX)) {
+            parallelCountX++;
+            seenLines.add(lineKeyX);
+         }
+         if (points[i].x === points[j].x && !seenLines.has(lineKeyY)) {
+            parallelCountY++;
+            seenLines.add(lineKeyY);
+         }
       }
    }
 
-   return { parallelToXCount, parallelToYCount };
+   return { parallelCountX, parallelCountY };
 }
 
-let numberOfPoints = 10; // n tane kordinat girilebilir
+// a) (1;2) , (3;7) , (0;6) , (1;1) , (-5,2), (1,5) , (3,-5)........
+let points = [
+   { x: 1, y: 2 },
+   { x: 3, y: 7 },
+   { x: 0, y: 6 },
+   { x: 1, y: 1 },
+   { x: -5, y: 2 },
+   { x: 1, y: 5 },
+   { x: 3, y: -5 },
+];
 
-let points = generateRandomPoints(numberOfPoints);
-console.log('Oluşturulan Noktalar:');
-points.forEach((point) => console.log(`x: ${point.x}, y: ${point.y}`));
+let parallelLineCount = findParallelLines(points);
+points.forEach((element) => {
+   console.log('x: ', element.x, 'y: ', element.y);
+});
 
-let { parallelToXCount, parallelToYCount } = checkLineParallels(points);
-console.log('X eksenine paralel benzersiz doğru sayısı:', parallelToXCount);
-console.log('Y eksenine paralel benzersiz doğru sayısı:', parallelToYCount);
+console.log(
+   'X eksenine paralel doğru sayısı:',
+   parallelLineCount.parallelCountX
+);
+console.log(
+   'Y eksenine paralel doğru sayısı:',
+   parallelLineCount.parallelCountY
+);
