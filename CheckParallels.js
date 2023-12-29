b3be5486fe7f8dfed2c3a5329ced92317e9b24a8
mmgumus <mahmut_muhammet@hotmail.com>
2023-12-29 12:47:59 +0300

    update

--- CheckParallels.js
+++ CheckParallels.js
@@ -1,3 +1,14 @@
+// a) (1;2) , (3;7) , (0;6) , (1;1) , (-5,2), (1,5) , (3,-5)........
+let points = [
+   { x: 1, y: 2 },
+   { x: 3, y: 7 },
+   { x: 0, y: 6 },
+   { x: 1, y: 1 },
+   { x: -5, y: 2 },
+   { x: 1, y: 5 },
+   { x: 3, y: -5 },
+];
+
 function findParallelLines(points) {
    let parallelCountX = 0;
    let parallelCountY = 0;
@@ -9,10 +20,12 @@ function findParallelLines(points) {
          let lineKeyX = `y:${points[i].y}-${points[j].y}`;
 
          if (points[i].y === points[j].y && !seenLines.has(lineKeyX)) {
+            // x paralelliği için y kordinatlarını karşılaştırıyorum
             parallelCountX++;
-            seenLines.add(lineKeyX);
+            seenLines.add(lineKeyX); // aynı doğru üzerindeki noktaları tekrar saymamak için
          }
          if (points[i].x === points[j].x && !seenLines.has(lineKeyY)) {
+            // y paralelliği için x kordinatlarını karşılaştırıyorum
             parallelCountY++;
             seenLines.add(lineKeyY);
          }
@@ -21,17 +34,6 @@ function findParallelLines(points) {
 
    return { parallelCountX, parallelCountY };
 }
-
-// a) (1;2) , (3;7) , (0;6) , (1;1) , (-5,2), (1,5) , (3,-5)........
-let points = [
-   { x: 1, y: 2 },
-   { x: 3, y: 7 },
-   { x: 0, y: 6 },
-   { x: 1, y: 1 },
-   { x: -5, y: 2 },
-   { x: 1, y: 5 },
-   { x: 3, y: -5 },
-];
 
 let parallelLineCount = findParallelLines(points);
 points.forEach((element) => {
